89003c260cd74955dc054085bbdeeebe95583031
Lajt <matti815@gmail.com>
2015-10-07 19:10:06 +0200

    few more commands

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var bot = require('alfred-teamspeak');
 var cfg = require('./cfg.js');
 var request = require('request');
 var easypedia = require("easypedia");
+var google = require('google');
+var moment = require('moment');
+
+require('moment-countdown');
+
+var Chance = require('chance'),
+    chance = new Chance();
 
 var adminName = 'definitely not lajt';
 var adminId = 2;
@@ -12,11 +19,13 @@ var botCid;
 
 bot.configure(cfg());
 
+
 var commands = [
 	{ name: 'kanapka', body: '[b]Kanapka[/b], malo [color=red]hp[/color]'},
 	{ name: 'lekarz', body: '[color=blue]Tajemniczy [b]lekarz[/b] odbierajacy porod bigosa. Z jakiegos powodu bigos nie lubi o nim mowic...[/color]'},
 	{ name: 'bigos', body: '\n[color=blue]I Like Cats They Meow:[/color] nah bro im too mad i got hacked\n[color=blue]I Like Cats They Meow:[/color] and i got 1 min more cooldown\n[b][color=green]kurtRolson(bigos):[/color] men i dont happy\n[color=green]kurtRolson(bigos):[/color] i too was hacker 3 month ago[/b][color=blue]'},
-	{ name: 'michalCWEL', body: '<15:25:36> "Zaq": Zgadzam sie tez!'}
+	{ name: 'michalCWEL', body: '<15:25:36> "Zaq": Zgadzam sie tez!'},
+	{ name: 'arijan', body: '[url=https://www.youtube.com/watch?v=vH4vdxTgefs]WYGIBASY[/url]'}
 ];
 
 bot.on('login', function() {
@@ -53,6 +62,23 @@ function apiCall(query, callback){
 		return callback(null, text.sections[0].content[0].text);
 	});
 
+}
+
+function googleSearch(query, callback){
+	google.resultPerPage = 5;
+	google.lang = 'pl';
+	google.tld = 'pl';
+	
+	query = query.join(' ').trim();
+	
+	console.log(query);
+	
+	google(query, function(err, next, links){
+		if(err){
+			return callback('error', null);
+		}
+		return callback(null, links[0]);
+	})
 }
 
 function findAdmin(){
@@ -93,6 +119,7 @@ function updateCommands(err, admin){
 						bot.sayChannel(lajt.body, channel);
 					});
 				})
+				// wiki command
 				bot.addChannelCmd('wiki', channel, function(t){
 						//console.log(t);
 						//bot.sayChannel('t: '+t.info.params+' d: '+t.detail.msg, channel);
@@ -104,6 +131,55 @@ function updateCommands(err, admin){
 								bot.sayChannel('[color=green]'+text+'[/color]', channel);							
 						})
 					});
+				// google command
+				bot.addChannelCmd('google', channel, function(t){
+						// t.info.params
+						googleSearch(t.info.params, function(err, adr){
+							if(err || adr.link === null){
+								bot.sayChannel('[color=red]Jakiś ten... Fejk.[/color]', channel);
+								return;
+							}
+							//bot.sayChannel('[url='+adr.link+']'+adr.title+'[/url]', channel);
+							bot.sayChannel('\n[url='+adr.link+']'+adr.title+'[/url]\n[color=green]'+ adr.description +'[/color]', channel);
+						})
+					});	
+					
+					// chance command
+					bot.addChannelCmd('roll', channel, function(t){
+						var num = chance.integer({min: -10, max: 100});
+						//console.log(t);
+						bot.sayChannel('[color=green][b]'+t.detail.client_nickname+'[/b][/color] [color=blue]wylosował [b]'+num+'.[/b][/color]', channel);
+					});
+					bot.addChannelCmd('kolor', channel, function(t){
+						var color = chance.color({format: 'hex'});
+						//console.log(t);
+						bot.sayChannel('[color='+color+']Losowy kolor: '+color, channel);
+					});
+					bot.addChannelCmd('name', channel, function(){
+						var name = chance.name();
+						var country = chance.country({ full: true });
+						bot.sayChannel('[color=green]Hi, my name is[/color] [color=blue][b]'+name+'.[/b][/color][color=green] I live in [/color][color=blue][b]'+country+'[/b][/color]');
+					})
+					
+					bot.addChannelCmd('sunrise', channel, function(){
+						var sunrise = moment("2016-07-19").countdown().toString();
+						bot.sayChannel('[color=green]Nastepny SUNRISE za: [b]'+sunrise+'[/b][/color] [b][color=red]BIGOS, SZYKUJ DUPSKO![/color][/b]');
+					});
+					
+					bot.addChannelCmd('czy', channel, function(t){
+						if(!t.info.params.length){
+							return;
+						}
+						var rnd = chance.integer({min: 0, max: 1});
+						if(rnd===0){
+							bot.sayChannel('[b][color=red]NIE[/color][/b]');
+						}
+						else{
+							bot.sayChannel('[b][color=green]TAK[/color][/b]');							
+						}
+					});
+				
+				
 				afterFirstRun = true;
 			})
 	})
